10dd8e08655f0dccc576b7ca9432732d2f833f6a
Ina <ill@knightscope.com>
2018-05-19 15:41:11 -0700

    Modified to allow only one file or one folder

--- commands/compress_to_s3.py
+++ commands/compress_to_s3.py
@@ -8,14 +8,12 @@ if __name__== "__main__":
     parser.add_argument('--ext', default='.tgz', help='The compression type')
     parser.add_argument('--bucket', default='logs.kittcar.com', help='S3 bucket to upload to')
     parser.add_argument('--profile', default='KITT', help='Your AWS configuration profile name')
-    parser.add_argument('dir', nargs='+', help='Path to the folder(s) to compress')
+    parser.add_argument('dir', help='Path to the folder to compress')
+
     args = parser.parse_args()
 
     compress = Compress(args.ext)
-
-    for path in args.dir:
-        compress.add(path)
-
+    compress.add(args.dir)
     compress.close()
     compress.upload(args.bucket, args.profile)
     compress.delete()
